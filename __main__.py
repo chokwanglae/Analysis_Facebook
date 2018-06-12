5b15cdadde8cfbd6287305ddecf2f6fe3749cfab
twooopark <twooo.park@gmail.com>
2018-06-12 13:47:59 +0900

    add preprocess

--- __main__.py
+++ __main__.py
@@ -1,6 +1,17 @@
 # analysis_fb 프로젝트의 실행 프로그램...
 import collect
+import analyze
+import visualize
 
 # print('run analysis_fb...')
 if __name__ == '__main__':
+    # 데이터 수집(collection)
     collect.crawling('jtbcnews', '2017-01-01', '2017-12-31')
+
+    # 데이터 분석(analyze)
+'''
+'''
+
+    # 데이터 시각화(visualize)
+'''
+'''
--- collect/crawler.py
+++ collect/crawler.py
@@ -1,13 +1,40 @@
-from analysis_fb.collect.api import api
+import os
+from datetime import datetime, timedelta
+from .api import api
 
-# 전처리
+RESULT_DIRECTORY = '__results__/crawling'
+
+
+# 전처리   # 이 함수는 외부로 보여지고 싶지 않은데? --> init과 main 부분 변경
 def preprocess_post(post):
-    pass
+    if 'shares' not in post:
+        post['count_shares'] = 0
+    else:                                              # shares라는 컬럼이 안에 있다면,
+        post['count_shares'] = post['shares']['count'] # count_shares로 새로 넣듬
+
+    # 전체 코멘트 수
+    if 'comments' not in post:
+        post['count_comments'] = 0
+    else:
+        post['count_comments'] = post['comments']['summary']['total_count']
+    # 데이터를 불러올 depth를 줄이는 단순한 전처리를 배워보았다.
+
+    # KST = UTC + 9
+    kst = datetime.strptime(post['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
+    kst + timedelta(hours=9)
+    post['created_time'] = kst.strftime('%Y-%m-%d %H:%M:%S')
 
 def crawling(pagename, since, until):
-    print("crawling " + pagename)
+    results = []
+    filename = '%s/%s_%s_%s.json' % (RESULT_DIRECTORY, pagename, since, until)
     for posts in api.fb_fetch_posts(pagename, since, until):
-        print(posts)
-        print(len(posts))
+        for post in posts:
+            preprocess_post(post)
+        results += posts
+    # save results to file(저장, 적재)
+    outfile = open(filename, 'w', encoding='utf-8')
 
+# 경로가 없으면 만들자
+if os.path.exists(RESULT_DIRECTORY) is False:
+    os.makedirs(RESULT_DIRECTORY)
 
